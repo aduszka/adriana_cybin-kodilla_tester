8cb375201c33b7d41530fbd9820af32815b50afa
Adriana Cybin <adriana.cybin@gmail.com>
2020-09-01 21:59:05 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myHairColour = "fiolet";
+        String bestColour = "czarny";
+        int numberOfMyCats = 0;
+        double myHeight = 1.69;
+        char buildingEntrance = 'C';
+        boolean isWoman = true;
+        System.out.println(myHairColour);
+        System.out.println(bestColour);
+        System.out.println(numberOfMyCats);
+        System.out.println(myHeight);
+        System.out.println(buildingEntrance);
+        System.out.println(isWoman);
+    }
+}

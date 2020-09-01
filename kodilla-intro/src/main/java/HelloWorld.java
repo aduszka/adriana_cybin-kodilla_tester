418d683d815890a43225b1758089e18f2b8aecb6
Adriana Cybin <adriana.cybin@gmail.com>
2020-09-01 21:24:58 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Ada");
+        System.out.println("25");
+        System.out.println("C");
+    }
+}

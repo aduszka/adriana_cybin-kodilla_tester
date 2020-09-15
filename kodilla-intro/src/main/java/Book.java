363771dc13b191c323cdd7a56b6b3990bb50e685
Adriana Cybin <adriana.cybin@gmail.com>
2020-09-16 00:44:13 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    private String author;
+    private String title;
+
+    private static String of(String author, String title) {
+        return author + " " + title;
+    }
+
+    public static void main(String[] args) {
+        String book = Book.of("Isaac Asimov","The Galaxy");
+        System.out.println(book);
+    }
+}
